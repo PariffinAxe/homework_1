26a183cdd8da487cc4f68546ec685eb6ac6c3461
PariffinAxe <michael-brown@talktalk.net>
2020-08-31 15:23:37 +0100

    my first commit

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+# set score to 6
+# user input guess from 1 to 10
+# if user is correct print "correct"
+# else print "wrong"
+
+score = "6"
+
+user_score = input("Guess a number from 1 to 10?   ")
+if user_score == score:
+    print("correct")
+else:
+    print("wrong")
